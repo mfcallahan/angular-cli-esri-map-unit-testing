d1132cef3d8c1ae61baf9780399e1861df8569dc
Matt Callahan <matthew.callahan@outlook.com>
2020-11-27 08:44:49 -0700

    import angular-cli-esri-map/esri-loader-cli-project app

---
+++ src/app/esri-map/esri-map.component.ts
@@ -0,0 +1,130 @@
+/*
+  Copyright 2019 Esri
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy
+} from "@angular/core";
+import { loadModules } from "esri-loader";
+import esri = __esri; // Esri TypeScript Types
+
+@Component({
+  selector: "app-esri-map",
+  templateUrl: "./esri-map.component.html",
+  styleUrls: ["./esri-map.component.scss"]
+})
+export class EsriMapComponent implements OnInit, OnDestroy {
+  @Output() mapLoadedEvent = new EventEmitter<boolean>();
+
+  // The <div> where we will place the map
+  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
+
+  /**
+   * _zoom sets map zoom
+   * _center sets map center
+   * _basemap sets type of map
+   * _loaded provides map loaded status
+   */
+  private _zoom = 10;
+  private _center: Array<number> = [0.1278, 51.5074];
+  private _basemap = "streets";
+  private _loaded = false;
+  private _view: esri.MapView = null;
+
+  get mapLoaded(): boolean {
+    return this._loaded;
+  }
+
+  @Input()
+  set zoom(zoom: number) {
+    this._zoom = zoom;
+  }
+
+  get zoom(): number {
+    return this._zoom;
+  }
+
+  @Input()
+  set center(center: Array<number>) {
+    this._center = center;
+  }
+
+  get center(): Array<number> {
+    return this._center;
+  }
+
+  @Input()
+  set basemap(basemap: string) {
+    this._basemap = basemap;
+  }
+
+  get basemap(): string {
+    return this._basemap;
+  }
+
+  constructor() {}
+
+  async initializeMap() {
+    try {
+      // Load the modules for the ArcGIS API for JavaScript
+      const [EsriMap, EsriMapView] = await loadModules([
+        "esri/Map",
+        "esri/views/MapView"
+      ]);
+
+      // Configure the Map
+      const mapProperties: esri.MapProperties = {
+        basemap: this._basemap
+      };
+
+      const map: esri.Map = new EsriMap(mapProperties);
+
+      // Initialize the MapView
+      const mapViewProperties: esri.MapViewProperties = {
+        container: this.mapViewEl.nativeElement,
+        center: this._center,
+        zoom: this._zoom,
+        map: map
+      };
+
+      this._view = new EsriMapView(mapViewProperties);
+      await this._view.when();
+      return this._view;
+    } catch (error) {
+      console.log("EsriLoader: ", error);
+    }
+  }
+
+  ngOnInit() {
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap().then(mapView => {
+      // The map has been initialized
+      console.log("mapView ready: ", this._view.ready);
+      this._loaded = this._view.ready;
+      this.mapLoadedEvent.emit(true);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this._view) {
+      // destroy the map view
+      this._view.container = null;
+    }
+  }
+}
