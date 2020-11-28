1579d1394189bb7ec69c6586fb5c741bd66f039f
Matt Callahan <matthew.callahan@outlook.com>
2020-11-28 09:06:23 -0700

    layout

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { loadModules } from 'esri-loader';
 import esri = __esri;
 
@@ -9,26 +9,41 @@ import esri = __esri;
 })
 export class MapComponent implements OnInit {
   @ViewChild('map', { static: false })
-  private mapRef?: ElementRef;
+  private mapElementRef?: ElementRef;
   mapView?: esri.MapView;
   map?: esri.Map;
+  defaultCenterLat: number;
+  defaultCenterLon: number;
+  defaultZoom: number;
+  defaultBaseMap: string;
 
-  constructor() {}
+  constructor() {
+    // Set default map center and zoom to continental USA
+    this.defaultCenterLat = 39.83;
+    this.defaultCenterLon = -98.58;
+    this.defaultZoom = 5;
+    this.defaultBaseMap = 'dark-gray';
+  }
 
-  async ngOnInit(): Promise<void> {
+  ngOnInit(): void {
+    this.initDefaultMap();
+  }
+
+  private async initDefaultMap(): Promise<void> {
     const [Map, MapView] = (await loadModules(['esri/Map', 'esri/views/MapView'])) as [
       esri.MapConstructor,
       esri.MapViewConstructor
     ];
 
     this.map = new Map({
-      basemap: 'dark-gray',
+      basemap: this.defaultBaseMap,
     });
+
     this.mapView = new MapView({
       map: this.map,
-      center: [-112.01, 33.44],
-      zoom: 12,
-      container: this.mapRef?.nativeElement,
+      center: [this.defaultCenterLon, this.defaultCenterLat],
+      zoom: this.defaultZoom,
+      container: this.mapElementRef?.nativeElement,
       ui: {
         components: ['attribution'],
       },
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule],
+  exports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule],
 })
 export class MaterialModule {}
