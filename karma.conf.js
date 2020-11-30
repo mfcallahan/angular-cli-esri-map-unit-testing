f610f2fc99ae909daeacae15f37d148f1979dbdb
Matt Callahan <matthew.callahan@outlook.com>
2020-11-30 06:23:19 -0700

    update debug configs

--- karma.conf.js
+++ karma.conf.js
@@ -40,6 +40,9 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browserNoActivityTimeout: 300000,
+    browserDisconnectTimeout: 300000,
+    browserDisconnectTolerance: 10,
   });
 };
