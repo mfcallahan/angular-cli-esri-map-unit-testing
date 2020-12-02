20599c62d8b52ed84ec695b19d1c48773e7c32cf
Matt Callahan <matthew.callahan@outlook.com>
2020-12-02 16:00:51 -0700

    remove unused classes

--- src/test/esri/fakeMapViewModule.ts
+++
@@ -1,9 +0,0 @@
-import { FakeModule } from './fakeModule';
-
-export class FakeMapViewModule extends FakeModule {
-  ui = {
-    add: () => {
-      return;
-    },
-  };
-}
--- src/test/esri/fakeModule.ts
+++
@@ -1,3 +0,0 @@
-export class FakeModule {
-  constructor() {}
-}
